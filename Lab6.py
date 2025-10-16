18606b05beca5d6158b0ccc2663f4a0befeb6f6c
Саша <150384647+MOSFETOW@users.noreply.github.com>
2025-10-16 13:32:50 +0300

    Add files via upload

---
+++ Lab6.py
@@ -0,0 +1,27 @@
+import datetime
+data = {}
+
+
+def log_call(add):
+
+    def wrapper(*args, **kwargs):
+
+        data.update({
+        "call_time":datetime.datetime.now().strftime("%X"),
+        "fun_name":"add",
+        "arguments":args
+        })
+        
+        return add(*args, **kwargs)
+    
+    return wrapper
+
+
+
+@log_call
+def add(a, b):
+    return a / b
+
+print(add(4778, 54))
+
+print(data)
