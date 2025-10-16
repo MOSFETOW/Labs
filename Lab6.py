4a5e2137f6b28559c11c3d8e90568acdb6c0848d
Саша <150384647+MOSFETOW@users.noreply.github.com>
2025-10-16 14:10:11 +0300

    Update Lab6.py

--- Lab6.py
+++ Lab6.py
@@ -1,27 +1,32 @@
 import datetime
 data = {}
 
-
-def log_call(add):
-
-    def wrapper(*args, **kwargs):
-
-        data.update({
-        "call_time":datetime.datetime.now().strftime("%X"),
-        "fun_name":"add",
-        "arguments":args
-        })
-        
-        return add(*args, **kwargs)
+def main():
+    def log_call(add):
     
-    return wrapper
+        def wrapper(*args, **kwargs):
+    
+            data.update({
+            "call_time":datetime.datetime.now().strftime("%X"),
+            "fun_name":"add",
+            "arguments":args
+            })
+            
+            return add(*args, **kwargs)
+        
+        return wrapper
+    
+    
+    
+    @log_call
+    def add(a, b):
+        return a / b
+    
+    print(add(4778, 54))
+    
+    
+    print(data)
 
 
-
-@log_call
-def add(a, b):
-    return a / b
-
-print(add(4778, 54))
-
-print(data)
+if __name__ == '__main__':
+    main()
