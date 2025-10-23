c108f7389c1b070defc34332e342b7681d253ba1
Саша <150384647+MOSFETOW@users.noreply.github.com>
2025-10-23 13:37:25 +0300

    Add files via upload

---
+++ Laba8.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+import telebot
+
+
+
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+
+
+bot=telebot.TeleBot(TOKEN)
+
+@bot.message_handler()
+def start_message(message):
+    print(message)
+    bot.send_message(message.chat.id,message.text)
+
+
+
+def main():
+    bot.polling()
+
+
+
+if __name__ == '__main__':
+    main()
