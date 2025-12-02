a0ba8d1f6dd6f757fda7612bf89512b70fbf59e2
Саша <150384647+MOSFETOW@users.noreply.github.com>
2025-12-03 01:43:34 +0200

    Update Generator.py

--- LABA 9/Generator.py
+++ LABA 9/Generator.py
@@ -10,20 +10,21 @@ def pair_generator(filename):
 
         pairs = []
 
-        # шукаємо пари тільки всередині одного слова
+
         for w in words:
             for i in range(len(w) - 1):
                 pairs.append(w[i] + w[i + 1])
 
-        # повертаємо тільки 3 пари
+
         yield pairs[:3]
 
     f.close()
 
 
-# ======= приклад використання =========
+# -------------приклад використання --------------
 def main():
     for p in pair_generator("text100.txt"):
         print(p)
 if __name__ == "__main__":
+
     main()
